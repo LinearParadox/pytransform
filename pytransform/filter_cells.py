f87f24b736381967d4867d0d2c9e8658172d4646
LinearParadox <alexei.martsinkovskiy@gmail.com>
2023-05-24 23:19:07 -0700

    Fitting a negative binomial GLM to genes, and using log umis as a latent covariate.

--- pytransform/filter_cells.py
+++ pytransform/filter_cells.py
@@ -9,3 +9,24 @@ def add_MAD(anndata, vars):
 
 #For mitochondrias specifically, we only care about the right end of the tail (positive values)
 
+
+#Our dataset has cycling cells so we
+
+import anndata as ad
+# import scanpy as sc
+#
+# # Define gene lists for S phase and G2/M phase
+# s_genes = ["gene1", "gene2", "..."]  # Replace with actual list of S-phase genes
+# g2m_genes = ["geneA", "geneB", "..."]  # Replace with actual list of G2M-phase genes
+#
+# def filter_cells_by_cycle_phase(anndata, phase_to_remove):
+#     # Annotate cell cycle phase
+#     sc.tl.score_genes_cell_cycle(anndata, s_genes, g2m_genes) #built-in scanpy function to annotate cell cycle phase
+#
+#     # Filter cells
+#     anndata = anndata[anndata.obs['phase'] != phase_to_remove, :]
+#
+#     return anndata
+#
+# # Filter out cells in S phase
+# filtered_anndata = filter_cells_by_cycle_phase(anndata, 'S')
--- pytransform/pytransform.py
+++ pytransform/pytransform.py
@@ -1,13 +1,24 @@
-import anndata as ad
+from anndata import AnnData
 import pandas as pd
 import numpy as np
 from math import inf
 import scanpy as sc
 import scipy
+import statsmodels.genmod.families as families
+from statsmodels.genmod.generalized_linear_model import GLM
+from statsmodels.tools.tools import add_constant
+from typing import Optional, Iterable, Tuple, Union, List, Literal, Dict
 
 
-def _get_model_pars(anndata, bins, latent_vars):
-    pass
+def _get_model_pars(anndata, bins, latent):
+    latent = np.log(anndata.X.mean(1))
+    latent = add_constant(np.log1p(anndata.X.mean(1)))
+    # np_regress = GLM(endog=down_sample_filt.X[:, 1].toarray(), exog=add_constant(latent), family= families.NegativeBinomial(alpha=1))
+    models = [x for x in range(0, 2000)]
+    for ind, n in enumerate(anndata.X.T):
+        models[ind] = GLM(endog=n.T.toarray(), exog=latent, family=families.NegativeBinomial(alpha=1)).fit()
+    return models
+    #Multicore doesn't seem to be necessary?
     # Implement poisson fitting
     #input anndata object, already filtered using the step one function
     # should ideally include latent vars too.
@@ -30,13 +41,52 @@ def _step1(anndata, sample, min_cells, num_genes):
     probs=kde.pdf(np.asarray(gmeans).flatten())
     down_sample_filt=down_sample_filt[:, down_sample_filt.var.sample(n=num_genes, weights=probs).index]
     log_gmeans = np.log10(np.expm1(down_sample_filt.X.log1p().mean(0)))
+
     return down_sample_filt, log_gmeans
 
 
 
 
+###
 
-
+# def normalize_pearson_residuals(
+#     adata: AnnData,
+#     model_pars: np.ndarray,
+#     *,
+#     clip: Optional[float] = None,
+#     check_values: bool = True,
+#     layer: Optional[str] = None,
+#     inplace: bool = True,
+#     copy: bool = False,
+# ) -> Optional[Dict[str, np.ndarray]]:
+#     if copy:
+#         if not inplace:
+#             raise ValueError("`copy=True` cannot be used with `inplace=False`.")
+#         adata = adata.copy()
+#
+#
+#     X = _get_obs_rep(adata, layer=layer)
+#     computed_on = layer if layer else 'adata.X'
+#
+#     # Use the provided model parameters (e.g., theta estimates) for each gene
+#     residuals = _pearson_residuals(X, model_pars, clip, check_values, copy=~inplace)
+#     settings_dict = dict(theta=model_pars, clip=clip, computed_on=computed_on)
+#
+#     if inplace:
+#         _set_obs_rep(adata, residuals, layer=layer)
+#         adata.uns['pearson_residuals_normalization'] = settings_dict
+#     else:
+#         results_dict = dict(X=residuals, **settings_dict)
+#
+#     return adata if copy else (results_dict if not inplace else None)
+##
 
 def pytransform(anndata, training_cell=inf, min_cells=5, num_genes=2000):
     pass
+    # #Initial filtering and sampling
+    # sampling_filtering = _step1(anndata,min_cells,num_genes)
+    # #Fit model parameters
+    # model_pars = _get_model_pars(sampling_filtering[0], bins=1)
+    # #Normalize the anndata object
+    # normalized_anndata = normalize_pearson_residuals(sampling_filtering,model_pars)
+    # return normalized_anndata
