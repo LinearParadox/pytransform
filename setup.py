762def782db73e4668c905001cf7a3defcf37eb7
brandonhn <61365182+brandonhn@users.noreply.github.com>
2023-05-26 19:58:11 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from setuptools import setup
+
+setup(
+    name="pytransform",  
+    version="0.1",
+    description="Additions to the automation of Single-Cell Analysis: A Python package with additional features for automated quality control in single-cell analysis",
+    author="Alexei Martinskovsky, Drew Hulsy, Brandon Nguyen",  
+    author_email="alexei.martsinkovskiy@gmail.com, drewhulsy@gmail.com, bhn002@.ucsd.edu",  
+    url="https://github.com/LinearParadox/pytransform",  
+    install_requires=[
+        'numpy>=1.20.0',
+        'scipy>=1.6.0',
+        'scanpy>=1.7.2',
+        'anndata>=0.7.6',
+        'pandas>=1.2.3',
+        'statsmodels>=0.12.0'
+    ]
+)
+
