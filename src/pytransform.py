e54bf0aacb4da853647969c57d67c5b3918f2363
LinearParadox <alexei.martsinkovskiy@gmail.com>
2023-06-07 19:28:16 -0700

    commented code

--- src/pytransform.py
+++ src/pytransform.py
@@ -18,6 +18,12 @@ _EPS = np.finfo(float).eps
 
 
 def _calc_model(anndata, latent):
+    """
+    Calculates the poisson regression model per gene. Implemented here for multiprocessing
+    :param anndata: anndata object
+    :param latent: latent variable to use. The latent variable is per cell. This is log(umi) by default/
+    :return: the overdispersion, slope, and intercept for the regression model
+    """
     models = [GLM(family=QuasiPoisson()) for _ in range(0, anndata.shape[1])]
     for n in range(0, anndata.shape[1]):
         logging.critical(n)
@@ -38,6 +44,13 @@ def _calc_model(anndata, latent):
     return theta, coefficient, intercept
 
 def _outliers(y, x, threshold):
+    """
+    Calculates if any of the parameters are outliets
+    :param y:  the variable to calculate the outlier against
+    :param x: the target to fit against
+    :param threshold: the outlier threshold
+    :return: THe parameters that are outliers
+    """
     kde = FFTKDE(kernel="gaussian", bw="ISJ").fit(x)
     width = (max(x)-min(x))*kde.bw / 2
     eps = _EPS * 10
@@ -48,6 +61,12 @@ def _outliers(y, x, threshold):
     return np.abs(np.vstack((scores1, scores2))).min(0)>threshold
 
 def _robust_scale(y, x, breaks): ## Possibly change later, implementation confusing
+    """
+    :param y: Metric to calculate scale for
+    :param x: Metric to scale against
+    :param breaks: number of bins
+    :return: outlier scale
+    """
     bins = np.digitize(x, breaks)
     unique_bins = np.unique(bins)
     res = np.zeros(bins.size)
@@ -58,6 +77,13 @@ def _robust_scale(y, x, breaks): ## Possibly change later, implementation confus
 
 
 def _regularize(anndata, model_pars, bw_adjust=3):
+    """
+    Regularizes all genes in the anndata object based on the previously fitted outlier models.
+    :param anndata: AN anndata object
+    :param model_pars: theta, slope, intercept
+    :param bw_adjust: The factor to scale the kernel density estimator by
+    :return: The anndata matrix with model pars estimated for all genes.
+    """
     anndata.var["Poisson"] = np.where((anndata.var["amean"] < 1e-3), True, False)
     model_pars.var["Poisson"] = np.where((model_pars.var["overdisp_fact"] <= 0)
                                          | (model_pars.var["theta"] == inf), True, False)
@@ -102,6 +128,11 @@ def _regularize(anndata, model_pars, bw_adjust=3):
 
 
 def _get_residuals(anndata, model_pars):
+    """
+    :param anndata: Anndata object
+    :param model_pars: slope, coefficient, overdispersion pars for the anndata
+    :return: Pearson residuals for the anndata object
+    """
     median = np.apply_along_axis(lambda v: np.median(v[np.nonzero(v)]), 0, anndata.X.toarray())
     min_var = (median/5)**2
     latent = np.array(np.log10(anndata.X.sum(1))).flatten()
@@ -129,6 +160,12 @@ def _get_residuals(anndata, model_pars):
 
 
 def _get_model_pars(anndata, num_workers):
+    """
+
+    :param anndata: anndata object
+    :param num_workers: number of cores to uses
+    :return: anndata with dispersion pars estimated for each gene in the object
+    """
     latent = np.array(np.log10(anndata.X.sum(1))).flatten()
     split = np.array_split(anndata.X.toarray(), num_workers, axis=1)
     pars_list = []
@@ -151,7 +188,14 @@ def _get_model_pars(anndata, num_workers):
     return anndata
 
 
-def _step1(anndata, min_cells,  num_cells, num_genes=inf):
+def _step1(anndata, min_cells,  num_cells, num_genes=2000):
+    """
+    :param anndata: anndata object
+    :param min_cells: minimum number of cells for a gene to be expressed in to be retained
+    :param num_cells: number of cells to use for the initial regression step
+    :param num_genes: The number of genes to estimate parameters for in the initial regression step
+    :return: an anndata object downsampled from the original anndata to num_cells cells and num_genes genes.
+    """
     sc.pp.filter_genes(anndata, min_cells=min_cells)
     down_sample = sc.pp.subsample(anndata, n_obs=min(num_cells, anndata.shape[0]), copy=True)
     sc.pp.filter_genes(down_sample, min_cells=1)
@@ -174,6 +218,23 @@ def _step1(anndata, min_cells,  num_cells, num_genes=inf):
 
 def pytransform(anndata, min_cells=5, num_genes=2000, num_cells=5000, workers=os.cpu_count()-1, inplace=True,
                 verbose=False, highly_variable=3000):
+    """
+    Performs pearson residual estimation on the anndata object. Please note that the initial regression estimation is
+    very compute intensive, therefore it is advised to not select too many genes for the num_genes parameter
+    :param anndata: an anndata object **NOTE: BATCH EFFECTS ARE CURRENTLY UNSUPPORTED. PLEASE SPLIT THE ANNDATA BEFORE
+    YOU APPLY PYTRANSFORM**
+    :param min_cells: The min number of cells a gene should be expressed in to not be filtered out. Default is 5
+    :param num_genes: Number of genes to use in the initial regression step. Default is 2000
+    :param num_cells: Number of cells to use in hte initial regression step. Default is 5000
+    :param workers: Number of CPU cores to use. Default is available_cores-1
+    :param inplace: Whether to modify the anndata object or return a copy.
+    :param verbose: Currently, unimplemented. In the future, will set the level of messages to output
+    :param highly_variable: The number of highly variable genes to retain in the final object. Set to 0 to retain all
+    genes. Note: scanpy's default highly variable gene selection is not designed to take in pearson residuals, and will
+    output strange results if used with these. If you intend to select highly variable genes, please use this function
+    instead.
+    :return: An anndata object if inplace=False, else nothing.
+    """
     if verbose:
         logging.basicConfig(level=logging.INFO)
     sub_samp = _step1(anndata, min_cells=min_cells, num_genes=num_genes, num_cells=num_cells)
@@ -208,8 +269,3 @@ def pytransform(anndata, min_cells=5, num_genes=2000, num_cells=5000, workers=os
 
 
 
-##
-#x_sq = adata.X.copy()
-#x_mean = adata.X.mean(0)
-#x_sq.data **= 2
-#genes_var = np.asarray(x_sq.mean(0)) - np.asarray(np.square(x_mean))
